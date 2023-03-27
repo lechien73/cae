1a3f248eef89f2277e4fbe8c356dd665ac17bf67
Matt Rudge <matt.rudge@codeinstitute.net>
2023-03-27 07:31:51 +0000

    Styling

--- js/content.js
+++ js/content.js
@@ -6,16 +6,16 @@
     const newRepo = document.getElementsByClassName("TableObject")[0];
 
 
-    if (root) {
+    if (root && currentWindow.includes("github.com")) {
 
         const buttonContainer = root.appendChild(document.createElement("div"));
         buttonContainer.setAttribute("class", "empty-icon position-relative ml-1 float-right");
 
-        buttonContainer.innerHTML = `<a class='btn btn-secondary' style='background-color: #7f3f98;' href='https://app.codeanywhere.com/#${currentWindow}' target='_blank'>Codeanywhere</a>`;
+        buttonContainer.innerHTML = `<a class='btn btn-primary' style='background-color: #7f3f98; color: #fff;' href='https://app.codeanywhere.com/#${currentWindow}' target='_blank'>Codeanywhere</a>`;
     } else {
         const newRepoButton = document.createElement("div");
         newRepoButton.setAttribute("class", "TableObject-item");
-        newRepoButton.innerHTML = `<a class='btn btn-sm btn-primary mr-1' href='https://app.codeanywhere.com/#${currentWindow}' target='_blank'>Codeanywhere</a>`;
+        newRepoButton.innerHTML = `<a class='btn btn-sm btn-primary mr-1' style='background-color: #7f3f98; color: #fff;' href='https://app.codeanywhere.com/#${currentWindow}' target='_blank'>Codeanywhere</a>`;
         newRepo.insertBefore(newRepoButton, newRepo.children[0]);
     }
 
