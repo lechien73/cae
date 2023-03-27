e5fb8bf14a333a195565b231fe26e31cfe6324a6
Matt Rudge <matt.rudge@codeinstitute.net>
2023-03-27 07:26:23 +0000

    Try fix filepaths

--- js/content.js
+++ js/content.js
@@ -11,11 +11,11 @@
         const buttonContainer = root.appendChild(document.createElement("div"));
         buttonContainer.setAttribute("class", "empty-icon position-relative ml-1 float-right");
 
-        buttonContainer.innerHTML = `<a class='btn btn-primary' href='https://app.codeanywhere.com/#${currentWindow}' target='_blank'>Codeanywhere</a>`;
+        buttonContainer.innerHTML = `<a class='btn btn-secondary' style='background-color: #7f3f98;' href='https://app.codeanywhere.com/#${currentWindow}' target='_blank'>Codeanywhere</a>`;
     } else {
         const newRepoButton = document.createElement("div");
         newRepoButton.setAttribute("class", "TableObject-item");
-        newRepoButton.innerHTML = `<a class='btn btn-small btn-primary' href='https://app.codeanywhere.com/#${currentWindow}' target='_blank'>Codeanywhere</a>`;
+        newRepoButton.innerHTML = `<a class='btn btn-sm btn-primary mr-1' href='https://app.codeanywhere.com/#${currentWindow}' target='_blank'>Codeanywhere</a>`;
         newRepo.insertBefore(newRepoButton, newRepo.children[0]);
     }
 
